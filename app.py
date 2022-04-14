e037895707b771c2c4b76f33f9c3814814e8a7b4
duvrdx <duvrdx@gmail.com>
2022-04-14 20:57:55 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+# Criando classes do Banco de Dados
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    completed = db.Column(db.Boolean)
+
+@app.route("/")
+def index():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    # Resgatando resposta do form
+    title = request.form["ftitle"]
+
+    # Criando novo item da classe Todo
+    new_todo = Todo(title=title, completed=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:id>")
+def update(id):
+    todo = Todo.query.filter_by(id = id).first()
+    todo.completed = not(todo.completed)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    todo = Todo.query.filter_by(id = id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+    
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
+    
